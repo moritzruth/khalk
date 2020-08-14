17dbc86953644a1b7dafd0d9cc91ee859760f1e2
Moritz Ruth <dev@moritzruth.de>
2020-08-14 18:52:12 +0200

    Fix publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+
 plugins {
     kotlin("jvm") version "1.3.72"
+    `java-library`
+    `maven-publish`
 }
 
 group = "de.moritzruth.khalk"
@@ -16,5 +19,13 @@ dependencies {
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("khalk") {
+            from(components["java"])
+        }
     }
 }
